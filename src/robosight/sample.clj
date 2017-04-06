bdc367263c928a59b3b91189ab5cfa16a83f4089
OJIMA Ryoji <rojima1@gmail.com>
2017-04-06 16:18:34 +0900

    refactored.

--- src/robosight/sample.clj
+++ src/robosight/sample.clj
@@ -5,13 +5,14 @@
 
 (defn -main
   [& args]
-  (doseq [objects-string (take-while identity (repeatedly read-line))]
-    (->> (repeatedly #(let [r (rand)]
-                        (cond
-                          (< r 0.4) {:function "forward" :parameter (+ (* (rand) 0.5) 0.5)}
-                          (< r 0.8) {:function "turn-to" :parameter (* Math/PI 2 (rand))}
-                          (< r 0.9) {:function "shoot"   :parameter (+ (* (rand) 5.0) 5.0)}
-                          :else     nil)))
-         (take 5)
-         (json/write-str)
-         (println))))
+  ((fn [] (when-let [objects-string (read-line)]
+            (->> (repeatedly #(let [r (rand)]
+                                (cond
+                                  (< r 0.4) {:function "forward" :parameter (+ (* (rand) 0.5) 0.5)}
+                                  (< r 0.8) {:function "turn-to" :parameter (* Math/PI 2 (rand))}
+                                  (< r 0.9) {:function "shoot"   :parameter (+ (* (rand) 5.0) 5.0)}
+                                  :else     nil)))
+                 (take 5)
+                 (json/write-str)
+                 (println))
+            (recur)))))
