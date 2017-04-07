e3f1782bf90e316d448fd40c688a9030aa1e2d20
OJIMA Ryoji <rojima1@gmail.com>
2017-04-07 10:42:40 +0900

    tuned.

--- src/robosight/sample.clj
+++ src/robosight/sample.clj
@@ -1,18 +1,47 @@
 (ns robosight.sample
-  (:require   (clojure.data [json :as json]))
+  (:require   (clojure.core [matrix :as matrix])
+              (clojure.data [json   :as json]))
   (:gen-class :name com.tail_island.robosight.Sample
               :main true))
 
+(def epsilon
+  (Math/pow 10 -8))
+
+(defn- angle
+  [[x y]]
+  (Math/atan2 y x))
+
+(defn- normalize-angle
+  [angle]
+  (robosight.sample/angle [(Math/cos angle) (Math/sin angle)]))
+
+(defn- anti-angle
+  [angle]
+  (normalize-angle (+ angle Math/PI)))
+
 (defn -main
   [& args]
-  ((fn [] (when-let [objects-string (read-line)]
-            (->> (repeatedly #(let [r (rand)]
-                                (cond
-                                  (< r 0.4) {:function "forward" :parameter (+ (* (rand) 0.5) 0.5)}
-                                  (< r 0.8) {:function "turn-to" :parameter (* Math/PI 2 (rand))}
-                                  (< r 0.9) {:function "shoot"   :parameter (+ (* (rand) 5.0) 5.0)}
-                                  :else     nil)))
-                 (take 5)
-                 (json/write-str)
-                 (println))
-            (recur)))))
+  ((fn [turn]
+     (when-let [state-string (read-line)]
+       (let [[friends enemies] (json/read-str state-string :key-fn keyword)]
+         (->> friends
+              (map (fn [friend]
+                     (case (mod turn 11)
+                       0 {:function "turn-to" :parameter (angle (matrix/sub (:center (->> enemies
+                                                                                          (filter #(> (:hp %) 0.0))
+                                                                                          (sort-by :hp)
+                                                                                          (first)))
+                                                                            (:center friend)))}
+                       1 {:function "shoot"   :parameter (+ (* (rand) 5.0) 5.0)}
+                       2 {:function "turn-to" :parameter (+ (anti-angle (angle (:center friend))) (- (rand Math/PI) (/ Math/PI 2)))}
+                       3 {:function "forward" :parameter 1.0}
+                       4 {:function "forward" :parameter 1.0}
+                       5 {:function "forward" :parameter 1.0}
+                       (let [anti-velocity-angle (anti-angle (angle (:velocity friend)))]
+                         (if (> (Math/abs (- (:direction friend) anti-velocity-angle)) epsilon)
+                           {:function "turn-to" :parameter anti-velocity-angle}
+                           {:function "forward" :parameter (matrix/length (:velocity friend))})))))
+              (json/write-str)
+              (println)))
+       (recur (inc turn))))
+   0))
