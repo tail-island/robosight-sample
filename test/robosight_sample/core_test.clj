76a607f4ef4a2d0b2a3177228c831b388bf04afd
OJIMA Ryoji <rojima1@gmail.com>
2017-04-12 12:02:23 +0900

    refactored.

--- test/robosight_sample/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns robosight-sample.core-test
-  (:require [clojure.test :refer :all]
-            [robosight-sample.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
